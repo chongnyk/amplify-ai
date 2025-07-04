7a87fcfe8f0a2454348f9fe07ab3286c0b17199c
chongnyk <chongnoelyk@gmail.com>
2025-07-04 13:11:51 -0400

    Changed background of vocab page

--- src/app/vocabulary/page.tsx
+++ src/app/vocabulary/page.tsx
@@ -48,7 +48,7 @@ function VocabularyPage() {
   })
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-100 dark:from-blue-800 dark:to-blue-200">
+    <div className="min-h-screen bg-gradient-to-br from-[#1e40af] to-[#60a5fa] dark:from-[#581c87] dark:to-[#7c3aed]">
       {/* <div className="min-h-screen bg-gray-50">
         <div className="bg-white border-b border-gray-200 px-4 py-3">
           <div className="flex items-center space-x-4">
